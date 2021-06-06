4c312422e013d17f0fa87f3d7445c268f43fca06
mikhailknyazev <michael.knyazev@gmail.com>
2021-06-07 00:49:37 +1000

    n4b_pod_cpu_stress_better

--- course/config/step3_descheduler.tf
+++ course/config/step3_descheduler.tf
@@ -1,14 +1,16 @@
 
+// Note: The "descheduler" has been disabled to give more room to pods of other demos in UI
+
 // See: https://github.com/kubernetes-sigs/descheduler/tree/master/charts/descheduler
-resource "helm_release" "descheduler" {
-  name       = "descheduler"
-  chart      = "descheduler"
-  repository = "https://kubernetes-sigs.github.io/descheduler"
-  version    = "0.20.0"
-  namespace  = "kube-system"
-  values = [templatefile("${path.module}/helm/values/descheduler.yaml",
-  {
-    threshold_priority_class_name = "privileged"
-    workload_logical_role_name    = local.workload_logical_role_name
-  })]
-}
+//resource "helm_release" "descheduler" {
+//  name       = "descheduler"
+//  chart      = "descheduler"
+//  repository = "https://kubernetes-sigs.github.io/descheduler"
+//  version    = "0.20.0"
+//  namespace  = "kube-system"
+//  values = [templatefile("${path.module}/helm/values/descheduler.yaml",
+//  {
+//    threshold_priority_class_name = "privileged"
+//    workload_logical_role_name    = local.workload_logical_role_name
+//  })]
+//}
