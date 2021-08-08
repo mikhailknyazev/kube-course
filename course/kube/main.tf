4348313748efc58910ce745193f709e26b96fd3e
mikhailknyazev <michael.knyazev@gmail.com>
2021-08-08 17:21:47 +1000

    v1.0.0

--- course/kube/main.tf
+++ course/kube/main.tf
@@ -17,7 +17,7 @@ module "vpc" {
 module "kube" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.1.0"
-  cluster_version = "1.20"
+  cluster_version = "1.21"
   cluster_name    = local.cluster_name
   subnets         = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
